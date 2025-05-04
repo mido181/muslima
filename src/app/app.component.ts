b33f95d6a3dc2228196249faffed8c217208b966
mo <vitch.michhh@gmail.com>
2025-05-04 08:13:33 +0300

    fgh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
