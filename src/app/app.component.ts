e04792d498628f0617bbe36c5fb4b6ead6920e77
mo <vitch.michhh@gmail.com>
2025-05-05 00:07:18 +0300

    complete component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { MatIcon } from "@angular/material/icon";
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [],
+  imports: [MatIcon],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
